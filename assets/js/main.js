4c3ab35ca08fe04173d792153de3656dfb54b7da
Nikola Jevric <46942531+NJevric@users.noreply.github.com>
2020-11-03 03:35:39 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,120 +1,126 @@
 window.onload=function(){
+  loadAnimacija();
 
-  // div circle nav
-
-  let objCircleNav = {
-    klasa : "active",
-    home : "home",
-    about : "aboutt",
-    work : "workk",
-    contact : "contactt",
-    arrow : "arrow",
-    page: "page",
-    social: "social"
-  };
-  
- 
-  window.addEventListener('scroll',function(){
-
-    let scroll = window.scrollY;
-    
-    let scrollNav = () => {
-      if(scroll>50){
-
-      }
-    }
-
-    let scrollArrow = () => {
-
-      let domArrow =  document.getElementById(objCircleNav.arrow);
-
-      if(scroll>150){
-        domArrow = domArrow.style.display='block';
-      }
-      else{
-        domArrow =  domArrow.style.display='none';
-      }
-      return domArrow;
-    }
-    
-    let scrollPageNav = () => {
-      if(scroll<900){
-        $(`#${objCircleNav.home}`).addClass(objCircleNav.klasa);
-        $(`#${objCircleNav.about}`).removeClass(objCircleNav.klasa);
-      }
-      if(scroll<1800 && scroll >= 900){
-        $(`#${objCircleNav.home}`).removeClass(objCircleNav.klasa);
-        $(`#${objCircleNav.about}`).addClass(objCircleNav.klasa);
-        $(`#${objCircleNav.work}`).removeClass(objCircleNav.klasa);
-      }
-      if(scroll<2700 && scroll >= 1800){
-        $(`#${objCircleNav.about}`).removeClass(objCircleNav.klasa);
-        $(`#${objCircleNav.work}`).addClass(objCircleNav.klasa);
-        $(`#${objCircleNav.contact}`).removeClass(objCircleNav.klasa);
-      }
-      if(scroll<3600 && scroll >= 2700){
-        $(`#${objCircleNav.work}`).removeClass(objCircleNav.klasa);
-        $(`#${objCircleNav.contact}`).addClass(objCircleNav.klasa);
-      }
-    }
-   
-    scrollPageNav();
-    scrollArrow();
-  })
-
-  const load = document.querySelectorAll('#loadSvg path');
-  console.log(load);
-  for(let i of load){
-    console.log(`Letter ${i} is ${i.getTotalLength()}`);
-  }
-  
-  // JS load animation
-  setTimeout(function(){
-    document.getElementById('loadSvg').classList.remove('load');
-    document.getElementById(objCircleNav.social).style.visibility = 'visible';
-    document.getElementById(objCircleNav.page).style.visibility = 'visible';
-    document.getElementById('main').style.visibility = 'visible';
-    document.getElementById(objCircleNav.arrow).style.visibility = 'visible';
-    document.getElementById('header').style.visibility = 'visible';  
-    document.getElementById('footer').style.visibility = 'visible';  
-    document.getElementById('load').style.display = 'none';  
-  },4000);
-
-  // jquery load animation
-  // setTimeout(function(){
-  //   $("#loadSvg").removeClass('load');
-  // },3100);
-   
-    
-  
-//   $(window).scroll(function () {
-//     var scroll = $(window).scrollTop();
-//     if(scroll<900){
-//       $(`#${objCircleNav.idHome}`).addClass(objCircleNav.klasa);
-//       $(`#${objCircleNav.idAbout}`).removeClass(objCircleNav.klasa);
-//     }
-//     if(scroll<1800 && scroll >= 900){
-//       $(`#${objCircleNav.idHome}`).removeClass(objCircleNav.klasa);
-//       $(`#${objCircleNav.idAbout}`).addClass(objCircleNav.klasa);
-//       $(`#${objCircleNav.idWork}`).removeClass(objCircleNav.klasa);
-//     }
-//     if(scroll<2700 && scroll >= 1800){
-//       $(`#${objCircleNav.idAbout}`).removeClass(objCircleNav.klasa);
-//       $(`#${objCircleNav.idWork}`).addClass(objCircleNav.klasa);
-//       $(`#${objCircleNav.idContact}`).removeClass(objCircleNav.klasa);
-//     }
-//     if(scroll<3600 && scroll >= 2700){
-//       $(`#${objCircleNav.idWork}`).removeClass(objCircleNav.klasa);
-//       $(`#${objCircleNav.idContact}`).addClass(objCircleNav.klasa);
-//     }
-    
-// });
-
-
-  //end div circle nav
-  
 }
 
-    
-    
+window.onscroll = function(){
+    scrollAnimacija();
+}
 
+let scrollAnimacija = () => {
+
+    let scroll = window.scrollY;
+  
+    try{
+  
+      let objCircleNav = {
+        klasa : "active",
+        home : "home",
+        about : "aboutt",
+        work : "workk",
+        contact : "contactt",
+        arrow : "arrow",
+       
+      };
+  
+      let scrollNav = () => {
+        if(scroll>50){
+    
+        }
+      }
+      let scrollArrow = () => {
+  
+        let domArrow =  document.getElementById(objCircleNav.arrow);
+    
+        if(scroll>150){
+          domArrow = domArrow.style.display='block';
+        }
+        else{
+          domArrow =  domArrow.style.display='none';
+        }
+        return domArrow;
+      }
+      let scrollPageNav = () => {
+        
+        const bodyHeight = () => {
+          const body = document.body,
+          html = document.documentElement;
+      
+          const height = Math.max( body.scrollHeight, body.offsetHeight, 
+            html.clientHeight, html.scrollHeight, html.offsetHeight );
+          
+          return height;
+        }
+        
+        if(scroll< (bodyHeight()*0.25) ){
+          $(`#${objCircleNav.home}`).addClass(objCircleNav.klasa);
+          $(`#${objCircleNav.about}`).removeClass(objCircleNav.klasa);
+        }
+        if(scroll < (bodyHeight()*0.5) && scroll >= (bodyHeight()*0.25) ){
+          $(`#${objCircleNav.home}`).removeClass(objCircleNav.klasa);
+          $(`#${objCircleNav.about}`).addClass(objCircleNav.klasa);
+          $(`#${objCircleNav.work}`).removeClass(objCircleNav.klasa);
+        }
+        if(scroll < (bodyHeight()*0.75) && scroll >= (bodyHeight()*0.5)){
+          $(`#${objCircleNav.about}`).removeClass(objCircleNav.klasa);
+          $(`#${objCircleNav.work}`).addClass(objCircleNav.klasa);
+          $(`#${objCircleNav.contact}`).removeClass(objCircleNav.klasa);
+        }
+        if(scroll < bodyHeight() && scroll >= (bodyHeight()*0.75)){
+          $(`#${objCircleNav.work}`).removeClass(objCircleNav.klasa);
+          $(`#${objCircleNav.contact}`).addClass(objCircleNav.klasa);
+        }
+      }
+
+      scrollNav();
+      scrollArrow();
+      scrollPageNav();
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
+
+let loadAnimacija = function(){
+  
+  try{
+    
+    let objLoadAnimacija = {
+      cssProperty : 'visible',
+      page: 'page',
+      social: 'social',
+      header: 'header',
+      main: 'main',
+      footer: 'footer',
+      arrow: 'arrow'
+    }
+
+    // pronadji path za stroke-dasharray
+    const load = document.querySelectorAll('#loadSvg path');
+    console.log(load);
+    for(let i of load){
+      console.log(`Path ${i} is ${i.getTotalLength()}`);
+    }
+
+    // JS load animation
+    setTimeout(function(){
+
+      document.getElementById('loadSvg').classList.remove('load');
+      document.getElementById(objLoadAnimacija.social).style.visibility = objLoadAnimacija.cssProperty;
+      document.getElementById(objLoadAnimacija.page).style.visibility = objLoadAnimacija.cssProperty;
+      document.getElementById(objLoadAnimacija.header).style.visibility = objLoadAnimacija.cssProperty;
+      document.getElementById(objLoadAnimacija.main).style.visibility = objLoadAnimacija.cssProperty;
+      document.getElementById(objLoadAnimacija.arrow).style.visibility = objLoadAnimacija.cssProperty;
+      document.getElementById(objLoadAnimacija.main).style.visibility = objLoadAnimacija.cssProperty;  
+      document.getElementById(objLoadAnimacija.footer).style.visibility = objLoadAnimacija.cssProperty;  
+      document.getElementById('load').style.display = 'none';  
+
+    },3500);
+  }
+
+  catch(err){
+    console.log(err);
+  }
+  
+}
