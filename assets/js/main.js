bfcde96bb42fd60fcb700b791f711c1d7060a091
Nikola Jevric <46942531+NJevric@users.noreply.github.com>
2020-11-24 01:49:25 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,68 +1,41 @@
 window.onload=function(){
   // document.querySelector('.navIco').style.color='#fff';
   // document.querySelector('#social a').style.color='#fff';
-  loadAnimacija();
-  navigacija();
+  try{
+    loadAnimacija();
+    navigacija();
+    projekti();
+    jezici();
+    socials();
+    page();
 
-  // VANTA.HALO({
-  //   el: "#work",
-  //   mouseControls: true,
-  //   touchControls: true,
-  //   gyroControls: true,
-  //   minHeight: 200.00,
-  //   minWidth: 200.00,
-  //   amplitudeFactor: 0.70,
-  //   xOffset: -0.3,
-  //   yOffset: -0.2,
-  //   size: .9
-   
-  // })
-
-  //   VANTA.HALO({
-  //   el: "#header",
-  //   mouseControls: true,
-  //   touchControls: true,
-  //   gyroControls: true,
-  //   minHeight: 200.00,
-  //   minWidth: 200.00,
-  //   amplitudeFactor: 0.70,
-  //   xOffset: 0.1,
-  //   yOffset: -0.1,
-  //   size: 0.75
-  // })
-  VANTA.NET({
-    el: "#header",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    points: 9.00,
-    spacing: 12.00
-  
-  });
-  VANTA.NET({
-    el: "#work",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00
-  });
-  // VANTA.NET({
-  //   el: "#tekst",
-  //   mouseControls: true,
-  //   touchControls: true,
-  //   gyroControls: false,
-  //   minHeight: 200.00,
-  //   minWidth: 200.00,
-  //   scale: 1.00,
-  //   scaleMobile: 1.00
-  // });
+    VANTA.NET({
+      el: "#header",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      points: 9.00,
+      spacing: 12.00
+    
+    });
+    VANTA.NET({
+      el: "#work",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00
+    });
+  }
+  catch(err){
+    console.log(err);
+  } 
 }
 
 window.onscroll = function(){
@@ -190,18 +163,11 @@ let loadAnimacija = function(){
      
     }
 
-    // pronadji path za stroke-dasharray
-    // const load = document.querySelectorAll('#loadSvg path');
-    // console.log(load);
-    // for(let i of load){
-    //   console.log(`Path ${i} is ${i.getTotalLength()}`);
-    // }
-
     let atribut = document.getElementById('loadAnimacija');
   
     let a = (i) => {
-      console.log(i++);
-
+      // console.log(i++);
+      i++;
       const maxVrednost = 100;
       if(i<maxVrednost){
         setTimeout(a,15,i);
@@ -214,12 +180,10 @@ let loadAnimacija = function(){
     // JS load animation
     setTimeout(function(){
 
-      // document.getElementById('load').classList.remove('load');
       document.getElementById(objLoadAnimacija.social).style.visibility = objLoadAnimacija.cssProperty;
       document.getElementById(objLoadAnimacija.page).style.visibility = objLoadAnimacija.cssProperty;
       document.getElementById(objLoadAnimacija.header).style.visibility = objLoadAnimacija.cssProperty;
       document.getElementById(objLoadAnimacija.main).style.visibility = objLoadAnimacija.cssProperty;
-     
       document.getElementById(objLoadAnimacija.main).style.visibility = objLoadAnimacija.cssProperty;  
       document.getElementById(objLoadAnimacija.footer).style.visibility = objLoadAnimacija.cssProperty;  
       document.getElementById('load').style.display = 'none';  
@@ -237,32 +201,253 @@ let loadAnimacija = function(){
 //Nav
 
 let navigacija = () => {
-  document.querySelector('.navIco').addEventListener('click',function(e){
-    e.preventDefault();
-    console.log('kliknuto');
-    document.querySelector('.small').classList.toggle('smallClick');
-    document.querySelector('.navOpen').classList.toggle('navOpened');
-    document.querySelector('.navTextResponsive').classList.toggle('clickText');
-    document.querySelector('.navIco').classList.toggle('animacija');
-    document.querySelector('.navIco').classList.toggle('animacijaNazad');
-    // let ispisNavigacije = () => {
-    //   let ispis = `<a class="" href="#about">About</a>
-    //   <a class="" href="#work">Work</a>
-    //   <a class="" href="#contact">Contact</a>`;
-    //   return document.querySelector('.navTextResponsive').innerHTML=ispis;
-    // }
-    // ispisNavigacije();
-    
-  });
+  try{
+    document.querySelector('.navIco').addEventListener('click',function(e){
+      e.preventDefault();
+      console.log('kliknuto');
+      document.querySelector('.small').classList.toggle('smallClick');
+      document.querySelector('.navOpen').classList.toggle('navOpened');
+      document.querySelector('.navTextResponsive').classList.toggle('clickText');
+      document.querySelector('.navIco').classList.toggle('animacija');
+      document.querySelector('.navIco').classList.toggle('animacijaNazad');
+      
+    });
+  }
+  catch(err){
+    console.log(err);
+  }
   
+  let navIspis = () => {
+    try{
+
+      let objNav = [{
+        link: "#about",
+        text: "About"
+      },
+      {
+        link: "#work",
+        text: "Work"
+      },
+      { link: "#contact",
+      text: "Contact"
+
+      }];
+
+      let ispis = (data) => {
+        let html = ``;
+        for(let i of data){
+          html+=`<a class="" href="${i.link}">${i.text}</a>`;
+        }
+        document.querySelector('.big').innerHTML=html;
+        document.querySelector('.navTextResponsive').innerHTML=html;
+      }
+      ispis(objNav);
+     
+    }
+    catch(err){
+      console.log(err);
+    }
+    
+  }
+  navIspis();
 }
+
 
 let projekti = () => {
   try{
 
+    let projekat = [{
+      id: 1,
+      src: 'assets/img/projekti/csa.png',
+      alt: 'CSA Sports Management',
+      naslov: 'CSA Sports Management',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat doloribus neque pariatur.',
+      tech: ['Bootstrap','Javascript','MySQL','PHP'],
+      links: {
+        domain: 'https://csasportsmanagement.com/',
+        code: 'https://github.com/NJevric/csaV2'
+      }
+    },
+    {
+      id: 2,
+      src: 'assets/img/projekti/tech.png',
+      alt: 'The Tech News',
+      naslov: 'The Tech News Blog',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat doloribus neque pariatur.',
+      tech: ['Bootstrap','Javascript','MySQL','PHP'],
+      links: {
+        domain: 'http://www.the-tech-news.epizy.com/cms/index.php',
+        code: 'https://github.com/NJevric/tech-news'
+      }
+    },
+    {
+      id: 3,
+      src: 'assets/img/projekti/fashionx.png',
+      alt: 'FashionX Web Shop',
+      naslov: 'FashionX Web Shop',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat doloribus neque pariatur.',
+      tech: ['Bootstrap','Javascript','AJAX'],
+      links: {
+        domain: 'https://njevric.github.io/fashionx/',
+        code: 'https://github.com/NJevric/fashionx'
+      }
+    },
+    {
+      id: 4,
+      src: 'assets/img/projekti/skver44.png',
+      alt: 'Skver 44',
+      naslov: 'Skver 44',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat doloribus neque pariatur.',
+      tech: ['Bootstrap','PHP'],
+      links: {
+        domain: 'http://skver44.rs/',
+        // code: 'www.cao1.com'
+      }
+    },
+    {
+      id: 5,
+      src: 'assets/img/projekti/blizu.png',
+      alt: 'Blizu Restobar',
+      naslov: 'Blizu Restobar',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat doloribus neque pariatur.',
+      tech: ['Bootstrap','PHP'],
+      links: {
+        domain: 'http://blizu.rs/',
+        // code: 'www.cao1.com'
+      }
+    },
+    {
+      id: 6,
+      src: 'assets/img/projekti/portfolio1.png',
+      alt: 'Portfolio First Version',
+      naslov: 'Portfolio 1st v.',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat doloribus neque pariatur.',
+      tech: ['HTML','Sass','XML'],
+      links: {
+        domain: 'http://blizu.rs/',
+        // code: 'www.cao1.com'
+      }
+    }
+  ];
+  // console.log(projekat);
+  let ispis = (data) => {
+
+    let html=``;
+
+    for(let i of data){
+      console.log(i);
+      html += ` 
+      <div class="img_wrap">
+        <img src="${i.src}" alt="${i.alt}" data-id="${i.id}" class="projekatImg"/>
+        <div class="img_desc">
+          <h3>${i.naslov}</h3>
+          <p class="desc">${i.desc}</p>
+          <ul class="tech">
+              `
+              for(let j of i.tech){
+                html+=`<li>${j}</li>`;
+              }
+
+              html+=`
+             
+          </ul>
+          <div class="links">
+              <a href="${i.links.domain}">Check it Out</a>
+              <a href="${i.links.code}">View Code</a>
+          </div>
+        </div>
+    </div>`
+    ;
+    
+    }
+    console.log(html);
+    document.getElementById('project').innerHTML=html;
+
+
+  }
+  console.log(projekat);
+  ispis(projekat);
+    
   }
   catch(err){
     console.log(err.message);
   }
 }
 
+let jezici = () => {
+  try{
+    let arrJezici = ['HTML5 / Bootstrap', 'CSS3 / Sass', 'Javascript(ES6)/ jQuery', 'PHP', 'SQL, MySQL', 'OOP(C#)'];
+    let html = '<ul>';
+    for(let i of arrJezici){
+      html +=`<li><ion-icon name="caret-forward-outline" class="aboutIcon"></ion-icon>${i}</li>`;
+    }
+    html+=`</ul>`;
+    document.querySelector('.lista').innerHTML=html;
+  }
+  catch(err){
+    console.log(err);
+  }
+}
+
+
+let socials = () => {
+  try{
+    let objSocials = [
+      {
+        link:'https://github.com/NJevric',
+        name: 'logo-github'
+      },
+      {
+        link:'https://www.instagram.com/n_jevric/',
+        name: 'logo-instagram'
+      },
+      {
+        link:'https://www.linkedin.com/in/nikola-jevric-1a64601b9/',
+        name: 'logo-linkedin'
+      },
+      {
+        link:'https://github.com/NJevric',
+        name: 'mail-outline'
+      }];
+    let html = ``;
+
+    objSocials.forEach(i=>{
+      html +=`  <a href="${i.link}"><ion-icon name="${i.name}" class="social"></ion-icon></a>`;
+    });
+    document.querySelector('#social').innerHTML=html;
+  }
+  catch(err){
+    console.log(err);
+  }
+}
+
+let page = () => {
+  try{
+    let objPage = [
+      {
+        link:'#',
+        id: 'home'
+      },
+      {
+        link:'#about',
+        id: 'aboutt'
+      },
+      {
+        link:'#work',
+        id: 'workk'
+      },
+      {
+        link:'#contact',
+        id: 'contactt'
+      }];
+    let html = ``;
+
+    objPage.forEach(i=>{
+      html +=`  <a href="${i.link}" class="link"><ion-icon name="ellipse-outline" class="page_circle" id="${i.id}"></ion-icon></a>`;
+    });
+    document.querySelector('#page').innerHTML=html;
+  }
+  catch(err){
+    console.log(err);
+  }
+}
